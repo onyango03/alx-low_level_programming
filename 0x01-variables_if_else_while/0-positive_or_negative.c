f16758d4b5a1858c219d48e20b0352457729efa6
onyango03 <emmanuelolweny02@gmail.com>
2022-09-12 01:53:49 -0700

    123

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 /* more headers goes there */
 /*
- *main - Entry point
- *this program will print is positive if number is greater than o
- *it will print is negative if n is less than 0
- *it will print zero if n is equal to 0
- *Return: Always 0 (Successs/correct)
- */
+*main - Entry point
+*this program will print is positive if number is greater than o
+*it will print is negative if n is less than 0
+*it will print zero if n is equal to 0
+*Return: Always 0 (Successs/correct)
+*/
 /* betty style docs for function main goes there */
 int main(void)
 {
@@ -22,7 +22,7 @@ printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero", n);
+printf("%d is zero\n", n);
 }
 else if (n < 0)
 {
